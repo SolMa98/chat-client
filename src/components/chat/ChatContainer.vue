0323f35a942fd62d9a6dcd1701f070e0921cf435
김동현 <zx12ehdgus12@gmail.com>
2025-03-23 20:42:17 +0900

    뷰 채팅창 메시지 전송 기능 구현

--- src/components/chat/ChatContainer.vue
+++ src/components/chat/ChatContainer.vue
@@ -1,15 +1,20 @@
+<script setup>
+  import "../../assets/js/common/sockjs.js";
+  import "../../assets/js/common/stomp.js";
+</script>
 <template>
   <div class="chat-container">
     <div class="chat-header">
       <input type="text" value="TALK" readonly>
     </div>
     <div class="chat-body" id="chatMessageView">
-
+      <div v-for="(message, index) in messages" :key="index" v-html="message">
+      </div>
     </div>
     <div class="chat-bottom">
       <label class="chat-bottom-label">
         <div class="chat-bottom-input-section">
-          <input placeholder="메시지를 입력하세요" value="" id="chatMessage" onkeydown="sendKeyDownEvent(event)">
+          <input placeholder="메시지를 입력하세요" value="" id="chatMessage" v-on:keydown="sendKeyDownEvent">
           <div class="chat-bottom-function-section">
             <div class="chat-bottom-function">
               <div class="chat-bottom-function-btn">
@@ -42,14 +47,200 @@
 
 <script>
   export default {
-    name: 'ChatContainer'
-  }
+    name: 'ChatContainer',
+    data() {
+      return {
+        messages: [],
+        stompClient: null,
+        senderKey: Math.random().toString(36).substr(2,11),
+        roomKey: ""
+      }
+    },
+    mounted() {
+      this.connect();
+    },
+    methods: {
+      connect(event) {
+        let socket = new WebSocket('ws://localhost:8080/ws');
+        this.stompClient  = Stomp.over(socket);
+        this.stompClient.connect({}, this.onConnected, this.onError);
+      },
 
-  import "../../assets/js/common/sockjs.js";
-  import "../../assets/js/common/stomp.js";
-  import "../../assets/js/chat/chat.js";
+      reConnect(roomId) {
+        if(this.stompClient && this.stompClient.connected){
+          this.stompClient.disconnect(function (){
+            console.log("소켓 연결이 끊어졌습니다.");
+
+            let socket = new WebSocket('ws://localhost:8080/ws');
+            this.stompClient = Stomp.over(socket);
+
+            this.stompClient.connect({}, function() {
+              console.log("새로운 STOMP 연결이 완료되었습니다.");
+              this.stompClient.subscribe('/sub/' + roomId, this.onMessageReceived);
+              this.roomKey = roomId;
+
+            }, this.onError);
+          });
+        }
+      },
+
+      onConnected() {
+        fetch("http://localhost:8080/chat/room", {
+          method: "POST"
+        })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Failed to create room");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          if (data) {
+            this.roomKey = data.roomId;
+            this.stompClient.subscribe('/sub/' + this.roomKey, this.onMessageReceived);
+
+            // 새로운 채팅방 생성 공지
+            this.stompClient.send("/pub/chat/new");
+          } else {
+            console.error('Data is undefined');
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+      },
+
+      onError(error) {
+        console.log('STOMP 연결 오류:', error);
+      },
+
+      onMessageReceived(payload) {
+        let message = JSON.parse(payload.body);
+
+        if(!(message.sender === "system")){
+          // 내가 보낸 메시지와 상대가 보낸 메시지 구분
+          if(message.senderKey === this.senderKey){
+            if(message.type === "file"){
+              this.makeFileMessage(message.fileUrl, "me");
+            }else{
+              this.makeMyMessage(message.content);
+            }
+          }else{
+            if(message.type === "file"){
+              this.makeFileMessage(message.fileUrl, "them");
+            }else{
+              this.makeThemMessage(message.content);
+            }
+          }
+
+          // 채팅창 스크롤 DOM이 업데이트 된 이후에 발동 되도록 설정
+          this.$nextTick(() => {
+            const chatBox = document.getElementById("chatMessageView");
+            chatBox.scrollTo({ top: chatBox.scrollHeight, behavior: "smooth" });
+          });
+        }
+      },
+
+      sendMessage(event) {
+        let messageInputElement = document.getElementById("chatMessage");
+
+        let message = {
+          roomId: this.roomKey,
+          sender: "agent",
+          senderKey: this.senderKey,
+          type: "message",
+          content: messageInputElement.value
+        }
+
+        messageInputElement.value = "";
+        this.stompClient.send("/pub/chat/message", {}, JSON.stringify(message));
+      },
+
+      sendFileMessage() {
+        let fileElement = document.getElementById("fileMessage");
+        fileElement.click();
+      },
+
+      handleFileChange(e) {
+        let imgFormData = new FormData();
+        imgFormData.append('file', e.target.files[0]);
+
+        fetch('http://localhost:8080/chat/upload/file', {
+          method: 'POST',
+          body: imgFormData
+        })
+        .then(response => response.json())
+        .then(data => {
+          let message = {
+            roomId: this.roomKey,
+            sender: "agent",
+            senderKey: this.senderKey,
+            content: "",
+            type: "file",
+            fileUrl: data.fileUrl
+          }
+
+          // WebSocket을 통해 파일 URL 전송
+          this.stompClient.send("/pub/chat/message", {}, JSON.stringify(message));
+        })
+        .catch(error => {
+          console.error("파일 업로드 실패:", error);
+        });
+      },
+
+      sendKeyDownEvent(event) {
+        if (event.key === 'Enter') {
+          this.sendMessage();
+        }
+      },
+
+      makeMyMessage(message) {
+        let myMessageHtml = `<div class="chat-me">
+                                        <div class="chat-me-message">
+                                            <p class="chat-me-name">me</p>
+                                            <p class="chat-me-text">${message}</p>
+                                        </div>
+                                        <div class="chat-me-profile-img"></div>
+                                    </div>`;
+        this.messages = [...this.messages, myMessageHtml];
+      },
+
+      makeFileMessage(file, sender) {
+        let senderClass = "me";
+        let myProfileHtml = `<div class="chat-${senderClass}-profile-img"></div>`;
+        let themProfileHtml = "";
+        if(sender === "them"){
+          senderClass = "them";
+          myProfileHtml = "";
+          themProfileHtml = `<div class="chat-${senderClass}-profile-img"></div>`;
+        }
+
+        let fileMessageHtml = `<div class="chat-${senderClass}">
+                                            ${themProfileHtml}
+                                            <div class="chat-${senderClass}-message">
+                                                <p class="chat-${senderClass}-name">User</p>
+                                                <img class="chat-${senderClass}-file" src="${file}"  alt="이미지"/>
+                                            </div>
+                                            ${myProfileHtml}
+                                        </div>`;
+        this.messages = [...this.messages, fileMessageHtml];
+      },
+
+      makeThemMessage(message) {
+        let themMessageHtml = `<div class="chat-them">
+                                        <div class="chat-them-profile-img"></div>
+                                        <div class="chat-them-message">
+                                            <p class="chat-them-name">User</p>
+                                            <p class="chat-them-text">${message}</p>
+                                        </div>
+                                    </div>`;
+
+        this.messages = [...this.messages, themMessageHtml];
+      }
+    }
+  }
 </script>
 
-<style scoped>
+<style>
   @import "../../assets/css/chat/chat.css";
 </style>
